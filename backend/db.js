5779209e9ca1bd565c41a2d84232b7bd12da3a1f
KANIKA <105192607+kanika126@users.noreply.github.com>
2024-04-15 23:54:06 +0530

    first commit

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,8 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/"
+const mongoURL = "mongodb+srv://kanika12602:Q20QaVJplrSL4Jrg@cluster0.o8sdgn1.mongodb.net/"
+const connectToMongo = () => {
+    mongoose.connect(mongoURL, () => {
+        console.log("Connected to Mongo Successfully");
+    })
+}
+module.exports = connectToMongo;
