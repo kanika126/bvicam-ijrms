1f6c38c58c42b436dbad95fded4b2a70429a94ff
KANIKA <105192607+kanika126@users.noreply.github.com>
2024-05-06 23:13:14 +0530

    recent

--- src/App.jsx
+++ src/App.jsx
@@ -50,10 +50,10 @@ function App() {
         <Route path="/currentissue1-2" element={<Currentisssue12></Currentisssue12>} />
         <Route path="/currentissue2-1" element={<Currentisssue21></Currentisssue21>} />
         <Route path="/feedback" element={<Feedback></Feedback>} />
-        {user && <Route path="/" exact element={<Main />} />}
+        {user && <Route path="/home" exact element={<Home />} />}
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
+			<Route path="/home" element={<Navigate replace to="/login" />} />
       </Routes>
       <Footer></Footer>
     </>
