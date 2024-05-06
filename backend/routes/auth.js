26d28b251e68d78352e5176365f623950afc5ba8
NIKHIL GUPTA <nikhil20530@iiitd.ac.in>
2024-05-06 23:30:41 +0530

    dummy changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -21,6 +21,7 @@ router.post("/", async (req, res) => {
 			return res.status(401).send({ message: "Invalid Email or Password" });
 
 		const token = user.generateAuthToken();
+		console.log("login done")
 		res.status(200).send({ data: token, message: "logged in successfully" });
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error" });
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Navigate } from "react-router-dom";
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from "./pages/Home/Home";
